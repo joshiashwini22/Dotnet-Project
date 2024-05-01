839fdef153cff1c402c805991d23dac0c87d7fe8
Ashwini Joshi <joshiashwini222@gmail.com>
2024-05-01 15:52:53 +0545

    Initial Commit
    Project Structure and database setup

---
+++ BisleriumProject/BisleriumProject.Infrastructures/DI/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using BisleriumProject.Infrastructures.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisleriumProject.Infrastructures.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("dev"),
+                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)), ServiceLifetime.Transient);
+            return services;
+        }
+    }
+}
---
+++ BisleriumProject/BisleriumProject.Infrastructures/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BisleriumProject.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BisleriumProject.Infrastructures.Persistence
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
